9180577f6cb797bdaf306841ed624bc3b68bb517
Renuthiru <renugathiru08@gmail.com>
2026-04-30 12:52:25 +0530

    Initial commit

--- data/users.php
+++ data/users.php
@@ -1,5 +1,14 @@
 <?php
 
+// This code returns an array.
+// It stores details of each user (username, email, password).
+
+// When this file is loaded using require_once,
+// the array is returned to index.php.
+
+// The returned data is stored in a variable (like $users)
+// and then used in the program for further processing. 
+
 return [
     [
         'username' => 'Rahul',
@@ -12,8 +21,8 @@ return [
         'password' => 'maria2024'
     ],
     [
-        'username' => 'Sameer',
-        'email'    => 'sameer@gmail.com',
+        'username' => 'Sameera',
+        'email'    => 'sameera@gmail.com',
         'password' => 'weak'
     ],
 ];
